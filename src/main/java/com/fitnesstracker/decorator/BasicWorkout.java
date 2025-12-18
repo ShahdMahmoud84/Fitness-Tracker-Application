f29bd3f4705427284b69a854d891c5464454b4af
omnia <oz1062003@gmil.com>
2025-12-18 13:14:36 +0200

    BasicWorkout

--- src/main/java/com/fitnesstracker/decorator/BasicWorkout.java
+++ src/main/java/com/fitnesstracker/decorator/BasicWorkout.java
@@ -8,6 +8,19 @@ package com.fitnesstracker.decorator;
  *
  * @author omnia zedan
  */
-public class BasicWorkout {
-    
-}
+public class BasicWorkout implements Workout {
+
+    @Override
+    public String getDescription() {
+        return "This cardio workout is designed to increase heart rate and improve cardiovascular endurance."
+                + " It includes dynamic exercises such as jumping jacks, running in place, high knees, "
+                + "and burpees. "
+                + "The workout helps burn calories, boost energy levels, "
+                + "and strengthen the heart and lungs. It is suitable for all fitness levels and can be "
+                + "adjusted by changing the intensity or duration of the exercises.";
+    }
+
+    @Override
+    public double getCalories() {
+        return 200;
+    }}
