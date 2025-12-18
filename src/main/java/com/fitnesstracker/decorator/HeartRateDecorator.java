2b00128aa1e8d0aae55be4bebdb214f2943db291
omnia <oz1062003@gmil.com>
2025-12-18 13:17:38 +0200

    HeartRateDecorator

--- src/main/java/com/fitnesstracker/decorator/HeartRateDecorator.java
+++ src/main/java/com/fitnesstracker/decorator/HeartRateDecorator.java
@@ -8,6 +8,19 @@ package com.fitnesstracker.decorator;
  *
  * @author omnia zedan
  */
-public class HeartRateDecorator {
-    
+public class HeartRateDecorator extends WorkoutDecorator {
+
+    public HeartRateDecorator(Workout workout) {
+        super(workout);
+    }
+
+    @Override
+    public String getDescription() {
+        return workout.getDescription() + "  Real-Time Heart Rate Monitoring ";
+    }
+
+    @Override
+    public double getCalories() {
+        return workout.getCalories() + 30;
+    }
 }
